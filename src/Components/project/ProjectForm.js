d19f5a70147437be5ff544c7a0530d13a75124ef
IagoFreitas12 <88669758+IagoFreitas12@users.noreply.github.com>
2022-05-24 00:29:22 -0300

    Using useEffect hook to manipulate options from DB inside ProjectForm.js

--- src/Components/project/ProjectForm.js
+++ src/Components/project/ProjectForm.js
@@ -1,9 +1,28 @@
+import { useState, useEffect } from 'react'
+
 import Input from '../form/Input'
 import styles from './ProjectForm.module.css'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 
 function ProjectForm ({btnText}) {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+        .then ((resp) => resp.json())
+        .then ((data) => {
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input 
@@ -21,6 +40,7 @@ function ProjectForm ({btnText}) {
             <Select 
                 text='Select the category'
                 name='category_id'
+                options={categories}
             />
             <SubmitButton text={btnText}/>
         </form>
